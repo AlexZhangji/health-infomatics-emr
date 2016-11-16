c17fce139bd39dc5b5c541dc3008519529aa92c7
zhang ji <zhan586@usc.edu>
2016-11-15 16:32:17 -0800

    add patient database generator

--- main/create_dummy_data.php
+++ main/create_dummy_data.php
@@ -20,7 +20,7 @@ function readTxt($filePath)
 {
     $file_handle = fopen($filePath, 'r');
     while (!feof($file_handle)) {
-        $line_of_text[] = fgets($file_handle,4096);
+        $line_of_text[] = fgets($file_handle, 4096);
         // $parts = explode('=', $line_of_text);
     }
     fclose($file_handle);
@@ -28,19 +28,16 @@ function readTxt($filePath)
     return $line_of_text;
 }
 
-
-$textDB = readTxt('tsv_files/icd10cm_order_2016.txt');
-
-foreach($textDB as $text){
-  $text = preg_split('/\s+/', $text);
-  print_r($text);
-}
+// $textDB = readTxt('tsv_files/icd10cm_order_2016.txt');
+//
+// foreach ($textDB as $text) {
+//     $text = preg_split('/\s+/', $text);
+//     print_r($text);
+// }
 
 // createDummyPatient(30);
 // importDiseaseDB();
 // createDummyVisit();
-
-
 
 function createDummyVisit()
 {
@@ -131,53 +128,67 @@ function getRandDate($start, $end)
 
 function createDummyPatient($numPpl)
 {
-    // templist
-  $genderListDummy = array('male', 'female');
-  // should use dictionary here
-  $cityListDummy = array('los angeles', 'new york city', 'barcelona', 'seattle', 'chengdu');
-    $stateListDummy = array('california', 'new york', 'catalonia', 'washington', 'sichuan');
+    $tableName = 'patient_data_gb';
 
-  // comiles to something diablo like : speedy Crab, defiant lizard
-  $nameAdjList = array('abandoned', 'able', 'absolute', 'adorable', 'adventurous', 'academic', 'acceptable', 'acclaimed', 'accomplished', 'accurate', 'aching', 'acidic', 'acrobatic', 'babyish', 'bad', 'back', 'baggy', 'bare', 'barren', 'basic', 'beautiful', 'belated', 'beloved',
-  'calculating', 'calm', 'candid', 'canine', 'capital', 'carefree', 'careful', 'careless', 'caring', 'damaged', 'damp', 'dangerous', 'dapper', 'daring', 'darling', 'dark', 'dazzling', 'dead', 'deadly', 'deafening', 'dear', 'dearest', 'fast', 'fat', 'fatal',
-  'fatherly', 'favorable', 'favorite', 'fearful', 'fearless', 'feisty', 'feline', 'female', 'feminine', 'few', 'radiant', 'ragged', 'rapid', 'rare', 'rash', 'raw', 'recent', 'reckless', 'rectangular', );
+    if ($result = sqlQuery("SHOW TABLES LIKE '$tableName'")) {
+        echo 'Table exists';
+    } else {
 
-    $nameAnimalList = array('Cat', 'Flea', 'Flowerpecker', 'Fly', 'Flying', 'Fish', 'Flying', 'Frog', 'Fossa', 'Fox', 'Frigatebird', 'Frog', 'Frogmouth', 'Fulmar', 'G', 'Galago', 'Gallinule', 'Gannet', 'Gar', 'Garter', 'Snake', 'Gaur', 'Gazelle',
-   'Gecko', 'Geoffroy', 's', 'Cat', 'Gerbil', 'Gerenuk', 'Giant', 'Panda', 'Giant', 'Tortoise', 'Gibbon', 'Gila', 'Monster', 'Giraffe', 'Gnu', 'Goat', 'Goatfish', 'Goldfish', 'Goose', 'Gopher', 'Goral', 'Gorilla', 'Gourami', 'Grackle', 'Grasshopper',
-   'Greater', 'Glider', 'Grebe', 'Green', 'Iguana', 'Grison', 'Grizzly', 'Bear', 'Groundhog', 'Grouse', 'Guanaco', 'Guinea', 'Pig', 'Gull', 'Gundi', 'H', 'Hamster', 'Harrier', 'Hartebeest', 'Hawaiian', 'Honeycreeper', 'Hawk', 'Hedgehog', 'Helmetshrike',
-   'Hermit', 'Crab', 'Heron', 'Himalayan', 'Tahr', 'Hippopotamus', 'Hissing', 'Cockroach', 'Honeyeater', 'Hornbill', 'Hornet', 'Horse', 'Hoverfly', 'Hummingbird', 'Hutia', 'Hyena', 'Hyrax', 'I', 'Iberian', 'Lynx', 'Ibex', 'Ibis', 'Icterid', 'Iguana',
-   'Impala', 'Insect', 'J', 'Jacana', 'Jack', 'Jackal', 'Jaguar', 'Jaguarundi', 'Jay', 'Jellyfish', 'Jerboa', 'Jungle', 'Cat', 'K', 'Kangaroo', 'Kangaroo', 'Rat', 'kerodon', 'Kestrel', 'King', 'Cobra', 'Kingbird', 'Kingfisher', 'Kinkajou', 'Kite',
-    'Kitten', 'Kiwi', 'Klipspringer', 'Knifefish', 'Koala', 'Kodiak', 'Bear', 'Kodkod', 'Koi', 'Komodo', 'Dragon', 'Kookaburra', 'Kowari', 'Kudu', 'Kultarr', 'L', 'Ladybug', 'Lamb', 'Lamprey', 'Lapwing', 'Leech', 'Lemming', 'Lemur', 'Leopard',
-     'Liger', 'Lion', 'Lionfish', 'Lizard', 'Llama', 'Loach', 'Lobster', 'Long', 'Tailed', 'Tit', 'Longspur', 'Loon', 'Loris', 'Lory', 'Lovebird', 'Lynx', 'Lyrebird', 'M', 'Macaw', 'Mallard', 'Mamba',
-     'Mammoth', 'Manakin', 'Manatee', 'Mandrill', 'Manta', 'Ray', );
+      echo 'create patient table';
+      // create tables;
+      sqlQuery("CREATE TABLE $tableName ".
+      '(id BIGINT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id),'.
+      'name VARCHAR(255), gender VARCHAR(255), DOB date, city_village VARCHAR(255),'.
+      'state_province VARCHAR(255), address_1 VARCHAR(255), address_2 VARCHAR(255), postal_num BIGINT, phone_num BIGINT;');
 
-  // get a list of IDs from DB, so that no duplicate names will be created
-  // $rawIdList = sqlQuery("SELECT id " .
-  //   "FROM `patient_data_gb` ");
+        // templist
+      $genderListDummy = array('male', 'female');
+      // should use dictionary here
+      $cityListDummy = array('los angeles', 'new york city', 'barcelona', 'seattle', 'chengdu');
+      $stateListDummy = array('california', 'new york', 'catalonia', 'washington', 'sichuan');
 
-  // $newId = end(array_values($rawIdList));
+      // comiles to something diablo like : speedy Crab, defiant lizard
+      $nameAdjList = array('abandoned', 'able', 'absolute', 'adorable', 'adventurous', 'academic', 'acceptable', 'acclaimed', 'accomplished', 'accurate', 'aching', 'acidic', 'acrobatic', 'babyish', 'bad', 'back', 'baggy', 'bare', 'barren', 'basic', 'beautiful', 'belated', 'beloved',
+      'calculating', 'calm', 'candid', 'canine', 'capital', 'carefree', 'careful', 'careless', 'caring', 'damaged', 'damp', 'dangerous', 'dapper', 'daring', 'darling', 'dark', 'dazzling', 'dead', 'deadly', 'deafening', 'dear', 'dearest', 'fast', 'fat', 'fatal',
+      'fatherly', 'favorable', 'favorite', 'fearful', 'fearless', 'feisty', 'feline', 'female', 'feminine', 'few', 'radiant', 'ragged', 'rapid', 'rare', 'rash', 'raw', 'recent', 'reckless', 'rectangular', );
 
-  for ($i = 0; $i < $numPpl; ++$i) {
-      $curDummy = [];
+            $nameAnimalList = array('Cat', 'Flea', 'Flowerpecker', 'Fly', 'Flying', 'Fish', 'Flying', 'Frog', 'Fossa', 'Fox', 'Frigatebird', 'Frog', 'Frogmouth', 'Fulmar', 'G', 'Galago', 'Gallinule', 'Gannet', 'Gar', 'Garter', 'Snake', 'Gaur', 'Gazelle',
+       'Gecko', 'Geoffroy', 's', 'Cat', 'Gerbil', 'Gerenuk', 'Giant', 'Panda', 'Giant', 'Tortoise', 'Gibbon', 'Gila', 'Monster', 'Giraffe', 'Gnu', 'Goat', 'Goatfish', 'Goldfish', 'Goose', 'Gopher', 'Goral', 'Gorilla', 'Gourami', 'Grackle', 'Grasshopper',
+       'Greater', 'Glider', 'Grebe', 'Green', 'Iguana', 'Grison', 'Grizzly', 'Bear', 'Groundhog', 'Grouse', 'Guanaco', 'Guinea', 'Pig', 'Gull', 'Gundi', 'H', 'Hamster', 'Harrier', 'Hartebeest', 'Hawaiian', 'Honeycreeper', 'Hawk', 'Hedgehog', 'Helmetshrike',
+       'Hermit', 'Crab', 'Heron', 'Himalayan', 'Tahr', 'Hippopotamus', 'Hissing', 'Cockroach', 'Honeyeater', 'Hornbill', 'Hornet', 'Horse', 'Hoverfly', 'Hummingbird', 'Hutia', 'Hyena', 'Hyrax', 'I', 'Iberian', 'Lynx', 'Ibex', 'Ibis', 'Icterid', 'Iguana',
+       'Impala', 'Insect', 'J', 'Jacana', 'Jack', 'Jackal', 'Jaguar', 'Jaguarundi', 'Jay', 'Jellyfish', 'Jerboa', 'Jungle', 'Cat', 'K', 'Kangaroo', 'Kangaroo', 'Rat', 'kerodon', 'Kestrel', 'King', 'Cobra', 'Kingbird', 'Kingfisher', 'Kinkajou', 'Kite',
+        'Kitten', 'Kiwi', 'Klipspringer', 'Knifefish', 'Koala', 'Kodiak', 'Bear', 'Kodkod', 'Koi', 'Komodo', 'Dragon', 'Kookaburra', 'Kowari', 'Kudu', 'Kultarr', 'L', 'Ladybug', 'Lamb', 'Lamprey', 'Lapwing', 'Leech', 'Lemming', 'Lemur', 'Leopard',
+         'Liger', 'Lion', 'Lionfish', 'Lizard', 'Llama', 'Loach', 'Lobster', 'Long', 'Tailed', 'Tit', 'Longspur', 'Loon', 'Loris', 'Lory', 'Lovebird', 'Lynx', 'Lyrebird', 'M', 'Macaw', 'Mallard', 'Mamba',
+         'Mammoth', 'Manakin', 'Manatee', 'Mandrill', 'Manta', 'Ray', );
 
-      $curName = $nameAdjList[array_rand($nameAdjList)].' '.$nameAnimalList[array_rand($nameAnimalList)];
+      // get a list of IDs from DB, so that no duplicate names will be created
+      // $rawIdList = sqlQuery("SELECT id " .
+      //   "FROM `patient_data_gb` ");
 
-      //  array_rand can pass in second parameter to output more entires
+      // $newId = end(array_values($rawIdList));
 
-      $curGender = $genderListDummy[array_rand($genderListDummy)];
-      $curCity = $cityListDummy[array_rand($cityListDummy)];
+      for ($i = 0; $i < $numPpl; ++$i) {
+          $curDummy = [];
 
-      // Using city's index in cityList to get corresponding state name.
-      // gosh.. why this looks so ugly...
+          $curName = $nameAdjList[array_rand($nameAdjList)].' '.$nameAnimalList[array_rand($nameAnimalList)];
 
-      $cityIndex = array_search($curCity, $cityListDummy);
-      $curState = array_values($stateListDummy)[$cityIndex];
-      $curDate = getRandDate(strtotime('1972-10-01'), strtotime('2015-12-31'));
+          //  array_rand can pass in second parameter to output more entires
 
-      $addNewPatient = sqlQuery('INSERT INTO patient_data_gb '.
-      '(name,gender, DOB ,city_village,state_province) '.
-      "VALUES('$curName', '$curGender','$curDate', '$curCity','$curState')");
-  }
+          $curGender = $genderListDummy[array_rand($genderListDummy)];
+          $curCity = $cityListDummy[array_rand($cityListDummy)];
+
+          // Using city's index in cityList to get corresponding state name.
+          // gosh.. why this looks so ugly...
+
+          $cityIndex = array_search($curCity, $cityListDummy);
+          $curState = array_values($stateListDummy)[$cityIndex];
+          $curDate = getRandDate(strtotime('1972-10-01'), strtotime('2015-12-31'));
+
+          $addNewPatient = sqlQuery('INSERT INTO patient_data_gb '.
+          '(name,gender, DOB ,city_village,state_province) '.
+          "VALUES('$curName', '$curGender','$curDate', '$curCity','$curState')");
+      }
+    }
 
     print_r('created '.$numPpl.' patient.');
 }
