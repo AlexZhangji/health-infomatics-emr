9d0d1d2577692725ea6645790f4d298959886ddf
zhang ji <zhan586@usc.edu>
2016-11-21 14:15:21 -0800

    reslove confilic in newVisit page, and stylize the autocomplete widget

--- main/newVisit.php
+++ main/newVisit.php
@@ -13,17 +13,16 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
  *
- * @package OpenEMR
  * @author  Brady Miller <brady@sparmy.com>
+ *
  * @link    http://www.open-emr.org
  */
-
 $fake_register_globals = false;
 $sanitize_all_escapes = true;
 
 /* Include our required headers */
-require_once('../globals.php');
-require_once("$srcdir/formdata.inc.php");
+require_once '../globals.php';
+require_once "$srcdir/formdata.inc.php";
 
 // Creates a new session id when load this outer frame
 // (allows creations of separate OpenEMR frames to view patients concurrently
@@ -40,14 +39,14 @@ if (isset($_POST['new_login_session_management'])) {
     session_regenerate_id(false);
 }
 
-$_SESSION["encounter"] = '';
+$_SESSION['encounter'] = '';
 
 // Fetch the password expiration date
 $is_expired = false;
 if ($GLOBALS['password_expiration_days'] != 0) {
     $is_expired = false;
     $q = (isset($_POST['authUser'])) ? $_POST['authUser'] : '';
-    $result = sqlStatement("select pwd_expiration_date from users where username = ?", array($q));
+    $result = sqlStatement('select pwd_expiration_date from users where username = ?', array($q));
     $current_date = date('Y-m-d');
     $pwd_expires_date = $current_date;
     if ($row = sqlFetchArray($result)) {
@@ -55,7 +54,7 @@ if ($GLOBALS['password_expiration_days'] != 0) {
     }
 
     // Display the password expiration message (starting from 7 days before the password gets expired)
-    $pwd_alert_date = date('Y-m-d', strtotime($pwd_expires_date . '-7 days'));
+    $pwd_alert_date = date('Y-m-d', strtotime($pwd_expires_date.'-7 days'));
 
     if (strtotime($pwd_alert_date) != '' &&
         strtotime($current_date) >= strtotime($pwd_alert_date) &&
@@ -69,153 +68,72 @@ if ($GLOBALS['password_expiration_days'] != 0) {
 
 if ($is_expired) {
     //display the php file containing the password expiration message.
-    $frame1url = "pwd_expires_alert.php";
-} else if (!empty($_POST['patientID'])) {
+    $frame1url = 'pwd_expires_alert.php';
+} elseif (!empty($_POST['patientID'])) {
     $patientID = 0 + $_POST['patientID'];
-    $frame1url = "../patient_file/summary/demographics.php?set_pid=" . attr($patientID);
-} else if ($GLOBALS['athletic_team']) {
-    $frame1url = "../reports/players_report.php?embed=1";
-} else if (isset($_GET['mode']) && $_GET['mode'] == "loadcalendar") {
-    $frame1url = "calendar/index.php?pid=" . attr($_GET['pid']);
-    if (isset($_GET['date'])) $frame1url .= "&date=" . attr($_GET['date']);
-} else if ($GLOBALS['concurrent_layout']) {
+    $frame1url = '../patient_file/summary/demographics.php?set_pid='.attr($patientID);
+} elseif ($GLOBALS['athletic_team']) {
+    $frame1url = '../reports/players_report.php?embed=1';
+} elseif (isset($_GET['mode']) && $_GET['mode'] == 'loadcalendar') {
+    $frame1url = 'calendar/index.php?pid='.attr($_GET['pid']);
+    if (isset($_GET['date'])) {
+        $frame1url .= '&date='.attr($_GET['date']);
+    }
+} elseif ($GLOBALS['concurrent_layout']) {
     // new layout
     if ($GLOBALS['default_top_pane']) {
         $frame1url = attr($GLOBALS['default_top_pane']);
     } else {
-        $frame1url = "main_info.php";
+        $frame1url = 'main_info.php';
     }
 } else {
     // old layout
-    $frame1url = "main.php?mode=" . attr($_GET['mode']);
+    $frame1url = 'main.php?mode='.attr($_GET['mode']);
 }
 
 $nav_area_width = $GLOBALS['athletic_team'] ? '230' : '130';
-if (!empty($GLOBALS['gbl_nav_area_width'])) $nav_area_width = $GLOBALS['gbl_nav_area_width'];
-
+if (!empty($GLOBALS['gbl_nav_area_width'])) {
+    $nav_area_width = $GLOBALS['gbl_nav_area_width'];
+}
 
 $patientId = trim($_GET['patientId']);
 if ($patientId) {
     echo $patientId;
 }
 
-$diseasesql = "SELECT name FROM disease_data_gb";
+$diseasesql = 'SELECT name FROM disease_data_gb';
     $diseaseresult = mysql_query($diseasesql);
 
     $disease_list = array();
-    while($row = mysql_fetch_array($diseaseresult))
-    {
+    while ($row = mysql_fetch_array($diseaseresult)) {
         $disease_list[] = $row['name'];
     }
 
 ?>
 
 <html>
-<<<<<<< HEAD
-  <head>
-
-
-
-
-
-
-    
-=======
 <head>
     <title>
         <?php echo text($openemr_name) ?>
->>>>>>> origin/master
     </title>
     <script type="text/javascript" src="../../library/topdialog.js"></script>
 
     <script language='JavaScript'>
-        <?php require($GLOBALS['srcdir'] . "/restoreSession.php"); ?>
+        <?php require $GLOBALS['srcdir'].'/restoreSession.php'; ?>
 
         // This counts the number of frames that have reported themselves as loaded.
         // Currently only left_nav and Title do this, so the maximum will be 2.
         // This is used to determine when those frames are all loaded.
         var loadedFrameCount = 0;
-
-<<<<<<< HEAD
-
       function allFramesLoaded() {
        // Change this number if more frames participate in reporting.
        return loadedFrameCount >= 2;
-=======
         function allFramesLoaded() {
             // Change this number if more frames participate in reporting.
             return loadedFrameCount >= 2;
->>>>>>> origin/master
-
         }
     </script>
     <link rel=stylesheet href="../themes/registration.css" type="text/css">
-<<<<<<< HEAD
-  </head>
-
-   <body  >
-
-
-      <h id = "heading">
-
-        <p id = "heading">
-          <a href="main_screen.php">
-            <img class="LoginLogo" src="/openemr-4.2.0/sites/default/images/Global_Brigades_Logo.png" alt="Login Image" style="width:200px;height:100px;">
-          </a>
-        </p>
-
-        </h>
-    <ul>
-      <li class = "left"><a href="main_screen.php">Home</a></li>
-      <li class = "left"><a href="#news">News</a></li>
-      <li class = "left"><a href="#contact">Contact</a></li>
-      <li class = "left"><a href="#tutorial">Tutorial</a></li>
-      
-      <li class = "right">
-       <a href="../logout.php" target="_top" class="css_button_small"  id="logout_link" onclick="top.restoreSession()" >
-      <span><?php echo htmlspecialchars( xl('Logout'), ENT_QUOTES) ?></span></a></td>
-      </li>
-      <li class = "right">User</li>
-      <li class = "right"><a href="#settings">Settings</a></li>
-
-
-    </ul>
-    </br>
-     
-        <style>
-        input[type=text], select {
-    border: 1px solid #ccc; 
-                padding: 7px 0px;
-                border-radius: 3px; 
-                padding-left:5px; 
-}
-input[type=submit] {
-    width: 20%;
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-}
-
-div.container {
-    width: 60%;
-    border: 1px solid gray;
-    background-color:#FFFFFF;
-}
-
-div.header {
-    width: 100%;
-    border: 1px solid gray;
-    background-color:#FFFFFF;
-    background-color: #20B2AA;
-}
-
-</style>
-=======
->>>>>>> origin/master
 
     <!-- Customized css -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -240,6 +158,9 @@ div.header {
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
     <link rel=stylesheet href="../themes/main_screen.css" type="text/css">
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  // <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
 </head>
 
@@ -386,80 +307,8 @@ div.header {
 
 </div>
 
-</body>
 
-<style>
-.visit-container{
-  margin-left:15%;
-  width: 70%;
-  padding: 10px;
-
-}
-
-</style>
-
-<<<<<<< HEAD
-
-      <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-  <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
-  
-
-        <div class="container">
-        <form action="md.php" method="get">
-                      <input type="submit" value="Back" 
-                               name="create_new_patient" id="backbutton" onclick="top.restoreSession()"  />
-        </form>
-        </br>
-        </br>
-        <form class = "new_visit" id = "new_visit" action = "createVisit.php" method = "POST">
-        <h2>Create Visit &nbsp;&nbsp;</h2>
-        <p><label for="visitDate">Date</label><input type="date" name="visit_date"/>&nbsp;</p>
-        <p>&nbsp;</p>
-        <p class="left"><label> Weight:&nbsp;</label> <input id="weight_field" class="" name="weight_field" size="10" type="int" value="" /><label>kg</label></p>
-        <p>&nbsp;</p>
-         <p class="left"><label> Height:&nbsp;</label> <input id="height_field" class="" name="height_field" size="10" type="int" value="" /><label>cm</label></p>
-        <p>&nbsp;</p>
-        <p class="left"><label>Temperature &nbsp;</label> <input id="temp_field" class="" name="temp_field" size="10" type="int" value="" /><label>°C</label></p>
-        <p>&nbsp;</p>
-        <p class="left"><label>Blood Pressure </label> 
-        <input id="b" class="" name="bph_field" size="10" type="int" value="" />
-        <label>/</label>
-        <input id="b" class="" name="bpl_field" size="10" type="int" value="" />
-        <p>&nbsp;</p>
-        <p class="left">&nbsp;<label>Pulse </label>
-        <input id="pulse_field" class="" name="pulse_field" size="10" type="int" value="" /><label>/min</label>
-        <p>&nbsp;</p>
-        <label>Respiratory Rate</label> 
-        <input id="respiratory_rate_field" class="" name="respiratory_rate_field" size="10" type="int" value="" /><label>/min</label></p>
-        <p>&nbsp;</p>
-        <p class="left"><label>Blood Oxygen Saturation </label> <input id="bos_field" class="" name="bos_field" size="10" type="int" value="" /><label>%</label></p>
-        <p class="left">&nbsp;</p>
-
-
-        <p class="left"><label>Cheif Complaint </label><input id="cc_field" class="" name="cc_field" size="40" type="text" value="" /> </p>
-        <p>&nbsp;</p>
-
-        <p class="left"><label>Signs and Symptoms </label><input id="symptoms_field" class="" name="symptoms_field" size="40" type="text" value="" /> </p>
-        <p>&nbsp;</p>
-
-        <p class="left"> <label>Diagnosis</label> <input id="diagnosis_field" class="" name="diagnosis_field" size="40" type="text" value="" /> </p>
-        <p>&nbsp;</p>
-
-        <p class="left"><label>Prescription </label><input id="rx_field" class="" name="rx_field" size="40" type="text" value="" /> </p>
-        <p>&nbsp;</p>
-
-        <label>Notes</label>
-        &nbsp;
-        <p class="left"> <textarea name="note_area" class="" rows="4" cols="40" type="text" value="" ></textarea> </p>
-
-        <input name='patient_id' type="hidden" value="<?php echo text($patientId);?>" >
-        <input type="submit" value="Submit" name = "submit_visit_button">
-        </form>
-        </div>
-
-    <script>
+<script>
 
     $(function() {
       var availableTags =  <?php echo json_encode($disease_list); ?>;
@@ -471,14 +320,35 @@ div.header {
 
 </script>
 
-  
+</body>
+
+<style>
+.visit-container{
+  margin-left:15%;
+  width: 70%;
+  padding: 10px;
+
+}
+
+.ui-menu-item{
+    background:white;
+    color:#2196F3;
+    display: block;
+}
+
+
+.ui-autocomplete {
+  color:blue;
+  width:200px;
+}
+
+</style>
+
+
     </body>
 
-  
 
-  
+
+
 
 </html>
-=======
-</html>
->>>>>>> origin/master
